74669c43309c3c3bdc6f66b05587edfd35ad08f3
Harrison Taylor <h.taylor@project89.org>
2015-03-31 13:33:17 -0400

    Added exercise 2, modded exercise 1.

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,8 @@
-puts 2
-puts 3
-puts 2 != 3
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
