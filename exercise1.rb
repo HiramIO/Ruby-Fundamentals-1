ad0683aeacfced4927025d8da1e2a4a06da8c348
Harrison Taylor <h.taylor@project89.org>
2015-03-31 14:15:23 -0400

    Updated poor old exercise 1. It wasn't getting enough love.

--- exercise1.rb
+++ exercise1.rb
@@ -2,7 +2,16 @@ puts "Enter name"
 name = gets.chomp
 puts "Your name is #{name}"
 
+puts "\n \n \n"
+
+
 puts "Hello\t\tworld"
 puts "Hello\b\b\b\b\bGoodbye world"
 puts "Hello\rStart over world"
 puts "1. Hello\n2. World"
+
+
+puts "\n \n \n"
+
+puts "So, before realizing that this exercise was more of an interative walkthrough, I pushed it up to GitHub. I figured it would be best to just make the best of it
+and put a little bit of extra love into it because I just finished everything and feel like writing a bit. Dont feel underappreciated, Exercise 1, we all love you <3"
