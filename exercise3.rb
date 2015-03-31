ec121b8ed253f4e0d2ee616d9bded97ad24ba796
Harrison Taylor <h.taylor@project89.org>
2015-03-31 13:59:27 -0400

    Added exercise 3, which returns a 'personalized' greeting based on the input of a name.

---
+++ exercise3.rb
@@ -0,0 +1,3 @@
+puts "Hello, my friend. My I inquire as to what your name may be?"
+name = gets.chomp
+puts "Why, it is a pleasure to meet you. Warmest of greetings and may you have a splediforous day, #{name}."
